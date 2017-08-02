93af3d9aba4fc00831f100d4fe07ae3e1ee79b98
Loremar <loremar.guimaraes@gmail.com>
2017-08-02 19:15:49 -0300

    Finished posts styling and configuration

--- wp-content/themes/simple/single.php
+++ wp-content/themes/simple/single.php
@@ -5,22 +5,17 @@
 				<?php while(have_posts()): the_post(); ?>
 					<article class="post">
 						<h3>
-							<a href="<?php the_permalink(); ?>">
-								<?php the_title(); ?>
-							</a>
+    						<?php the_title(); ?>
 						</h3>
 						<div class="meta">
 							Created By <?php the_author(); ?> on <?php the_date('l jS \of F Y h:i:s A'); ?>
-						</div>
-						<?php if(has_post_thumbnail()) : ?>
+                        </div>
+                        <?php if(has_post_thumbnail()) : ?>
 							<div class="post-thumbnail">
 								<?php the_post_thumbnail() ?>
 							</div>
 						<?php endif; ?>
-						<?php the_excerpt(); ?>
-						<!-- Go to functions to customize how many words you want in the_excerpt -->
-						<br>
-						<a class="button" href="<?php the_permalink(); ?>">Read More</a>
+						<?php the_content(); ?>
 					</article>
 				<?php endwhile; ?>
 			<?php else: ?>
